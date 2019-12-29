c3fa8a61ea74b1899e262091e0845a265dd42299
yuta <ka1357amnbpdr@gmail.com>
2019-12-30 00:02:26 +0900

    [add]履歴とその計算を行った時間の表示機能の追加

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ var app = new Vue({
     el: '#app',
     data: {
         ans: '',
-        formula:'',
+        formula: '',
         now: new Date(),
-        items: [
-            [7, 8, 9,'-','','√'],
-            [4, 5, 6, '/','','x^2'],
-            [1, 2, 3, '*','','log'],
-            [0,'00','+','=','','sin']
+        cal_historys: [],
+        buttons: [
+            [7, 8, 9, '-', '', '√'],
+            [4, 5, 6, '/', '', 'x^2'],
+            [1, 2, 3, '*', '', 'log'],
+            [0, '00', '+', '=', '', 'sin']
         ],
     },
     computed: {
@@ -20,20 +21,25 @@ var app = new Vue({
         calc: function (cmd) {
             if (cmd === '=') {
                 this.ans = eval(this.formula);
+                this.cal_historys.push(new Array(this.formula + "=" + this.ans, new Date()));
             } else if (cmd === 'C') {
                 this.ans = 0;
                 this.formula = 0;
             } else if (cmd === '√') {
                 this.ans = Math.sqrt(eval(this.formula));
-            } else if (cmd === 'x^2'){
+                this.push_history("√" + this.formula + "=" + this.ans, new Date());
+            } else if (cmd === 'x^2') {
                 this.ans = Math.pow(this.formula, 2);
+                this.push_history(this.formula  + "^2 "+ "=" + this.ans, new Date());
             } else if (cmd === 'log') {
                 this.ans = Math.log(eval(this.formula));
+                this.push_history("log"+this.formula + "=" + this.ans, new Date());
             } else if (cmd === 'sin') {
-                this.ans = Math.sin(eval(this.formula));
-            }else {
+                this.ans = Math.sin(eval(this.formula) / 180 * Math.PI);
+                this.push_history("sin(" + this.formula +")"+ "=" + this.ans, new Date());
+            } else {
                 this.formula += cmd;
             }
         }
-    },
+    }
 });
